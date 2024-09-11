67041f6a4afde808332446ff0651c0458369a3d7
ulumarul <112668758+ulumarul@users.noreply.github.com>
2024-09-11 12:50:03 +0700

    Upload

---
+++ app/src/main/java/com/papb/tugasm2papb/ui/theme/Type.kt
@@ -0,0 +1,24 @@
+package com.papb.tugasm2papb.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.sp
+    )
+)
